c145fe5c5968048f4ee1f92e5539bdf6bf17cca8
Hamza Hammouch <41025939+hhammouch@users.noreply.github.com>
2024-01-12 17:35:11 +0100

    Update nom-win.sh

--- nom-win.sh
+++ nom-win.sh
@@ -20,15 +20,6 @@ read -p "Paste Ngrok Authtoken: " CRP
 ./ngrok authtoken $CRP 
 
 clear
-echo "Repo: https://github.com/hhammouch/phordp/tree/main"
-echo "======================="
-echo "choose ngrok region (for better connection)."
-echo "======================="
-echo "us - United States (south carolina)"
-echo "eu - Europe (belgium)"
-echo "ap - Asia/Pacific (Singapore)"
-echo "sa - South America (Sao Paulo)"
-read -p "choose ngrok region: " CRP
 ./ngrok tcp --region $CRP 4000 &>/dev/null &
 sleep 1
 if curl --silent --show-error http://127.0.0.1:4040/api/tunnels  > /dev/null 2>&1; then echo OK; else echo "Ngrok Error! Please try again!" && sleep 1 && goto ngrok; fi
