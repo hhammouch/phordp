520a412dac53e565a63cb79b636accfca8fc6297
Hamza Hammouch <41025939+hhammouch@users.noreply.github.com>
2024-03-09 23:50:43 +0100

    Update ngrok.sh

--- ngrok.sh
+++ ngrok.sh
@@ -8,5 +8,5 @@ rm -rf ngrok ngrok.zip ng.sh > /dev/null 2>&1
 echo "======================="
 echo "Downloading ngrok..."
 echo "======================="
-wget -O ngrok.tgz https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz > /dev/null 2>&1
+wget -O ngrok.tgz https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz
 tar -xvzf ngrok.tgz > /dev/null 2>&1
